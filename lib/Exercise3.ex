d03c261a0c01f271fb5ad9e8db5698a984b44a71
Alexandre C <beejayvolts@gmail.com>
2025-12-04 22:55:32 +0100

    feat(Exercise3): ex02

--- lib/Exercise3.ex
+++ lib/Exercise3.ex
@@ -1,37 +1,86 @@
 defmodule Exercise3 do
+  @spec get_roll_indexes(list(String.t())) :: list(non_neg_integer())
+  defp get_roll_indexes(lines) do
+    lines
+    |> Enum.join()
+    |> String.split("")
+    |> Enum.with_index()
+    |> Enum.filter(fn {char, _index} -> char === "@" end)
+    |> Enum.map(fn {_char, index} -> index - 1 end)
+  end
+
   @spec get_walls_around(list(String.t()), non_neg_integer(), non_neg_integer()) :: integer()
   defp get_walls_around(lines, x, y) do
     [
-      {-1, -1}, {-1, 0}, {-1, 1},
-      {0, -1}, {0, 1},
-      {1, -1}, {1, 0}, {1, 1}
+      {-1, -1},
+      {-1, 0},
+      {-1, 1},
+      {0, -1},
+      {0, 1},
+      {1, -1},
+      {1, 0},
+      {1, 1}
     ]
     |> Enum.filter(fn {dir_x, dir_y} -> Utils.at(lines, x + dir_x, y + dir_y) === "@" end)
     |> length()
   end
 
-  @spec is_roll_acessible_by_forklift?(list(String.t()), integer()) :: boolean()
-  defp is_roll_acessible_by_forklift?(lines, index) do
+  @spec is_roll_accessible_by_forklift?(list(String.t()), integer()) :: boolean()
+  defp is_roll_accessible_by_forklift?(lines, index) do
     x = rem(index, lines |> hd() |> String.length())
     y = floor(index / length(lines))
 
     get_walls_around(lines, x, y) < 4
   end
 
+  @spec convert_rolls_to_positions(list(String.t())) :: list(Utils.non_neg_vector())
+  defp convert_rolls_to_positions(lines) do
+    width = lines |> hd() |> String.length()
+
+    lines
+    |> get_roll_indexes()
+    |> Enum.map(fn index -> {rem(index, width), floor(index / length(lines))} end)
+  end
+
+  @spec is_roll_position_accessible_by_forklift?(
+          list(Utils.non_neg_vector()),
+          Utils.non_neg_vector()
+        ) :: boolean()
+  defp is_roll_position_accessible_by_forklift?(positions, {x, y}) do
+    positions
+    |> Enum.filter(fn {other_x, other_y} -> abs(other_x - x) <= 1 && abs(other_y - y) <= 1 end)
+    |> length() < 5
+  end
+
+  @spec remove_rolls(list(Utils.non_neg_vector())) :: non_neg_integer()
+  defp remove_rolls(positions) do
+    to_be_kept =
+      positions
+      |> Enum.filter(fn current ->
+        !is_roll_position_accessible_by_forklift?(positions, current)
+      end)
+
+    to_be_removed = length(positions) - length(to_be_kept)
+
+    if to_be_removed === 0 do
+      0
+    else
+      to_be_removed + remove_rolls(to_be_kept)
+    end
+  end
+
   @spec ex1(list(String.t())) :: integer()
   def ex1(lines) do
     lines
-    |> Enum.join()
-    |> String.split("")
-    |> Enum.with_index()
-    |> Enum.filter(fn {char, _index} -> char === "@" end)
-    |> Enum.filter(fn {_char, index} -> is_roll_acessible_by_forklift?(lines, index - 1) end)
+    |> get_roll_indexes()
+    |> Enum.filter(fn index -> is_roll_accessible_by_forklift?(lines, index) end)
     |> length()
   end
 
   @spec ex2(list(String.t())) :: integer()
   def ex2(lines) do
     lines
-    0
+    |> convert_rolls_to_positions()
+    |> remove_rolls()
   end
 end
--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule AdventOfCode2025.MixProject do
 
   def application do
     [
-      mod: {AdventOfCode2025, ["--exercises", "0-1"]},
+      mod: {AdventOfCode2025, ["--exercises", "2-3"]},
       extra_applications: [:logger]
     ]
   end
